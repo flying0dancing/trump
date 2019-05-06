106b84c6c9f186aaf09117e8705460d54685d98c
Kun Shen <Kun.Shen@lombardrisk.com>
2019-05-06 16:29:58 +0800

    update IConfigCmd

--- trump-biz/src/main/java/com/lombardrisk/test/IConfigCmd.java
+++ trump-biz/src/main/java/com/lombardrisk/test/IConfigCmd.java
@@ -8,4 +8,18 @@ public interface IConfigCmd {
 	Boolean ICCB_RERUN=System.getProperties().containsKey("rerun");//rerun
 	String ICC_RERUNCONTENT=ICCB_RERUN?(Strings.isNullOrEmpty(System.getProperty("rerun"))?"all":System.getProperty("rerun").toLowerCase()):null;
 
+
+	//adding for ci
+	String ICC_TESTQA_SOURCE_POOL=System.getProperties().containsKey("testqa.source.pool")?System.getProperty("testqa.source.pool").replace("\\", "/"):null;
+	String ICC_TESTQA_SCENARIOS_POOL=System.getProperties().containsKey("testqa.scenarios.pool")?System.getProperty("testqa.scenarios.pool").replace("\\", "/"):null;
+	String ICC_TESTQA_TARGET_PATH=System.getProperties().containsKey("testqa.target.path")?System.getProperty("testqa.target.path").replace("\\", "/"):null;
+
+	String ICC_MAIL_HOSTNAME=System.getProperties().containsKey("mail.hostname")?System.getProperty("mail.hostname").replace("\\", "/"):null;
+	String ICC_MAIL_PORT=System.getProperties().containsKey("mail.port")?System.getProperty("mail.port").replace("\\", "/"):null;
+	String ICC_MAIL_SENDERADDRESS=System.getProperties().containsKey("mail.senderAddress")?System.getProperty("mail.senderAddress").replace("\\", "/"):null;
+	String ICC_MAIL_SENDERPASSWORD=System.getProperties().containsKey("mail.senderPassword")?System.getProperty("mail.senderPassword").replace("\\", "/"):null;
+	String ICC_MAIL_RECEIVER_TOLIST=System.getProperties().containsKey("mail.receiver.toList")?System.getProperty("mail.receiver.toList").replace("\\", "/"):null;
+	String ICC_MAIL_RECEIVER_CCLIST=System.getProperties().containsKey("mail.receiver.ccList")?System.getProperty("mail.receiver.ccList").replace("\\", "/"):null;
+	String ICC_MAIL_SUBJECT_PREFIX=System.getProperties().containsKey("mail.subject.prefix")?System.getProperty("mail.subject.prefix").replace("\\", "/"):null;
+
 }
--- trump-biz/src/main/java/com/lombardrisk/test/TestManager.java
+++ trump-biz/src/main/java/com/lombardrisk/test/TestManager.java
@@ -53,6 +53,7 @@ public class TestManager extends TestBase implements IComFolder {
 	 @BeforeSuite
 	  public void beforeSuite() {
 		 report("suite start.");
+		 System.out.println(System.getProperty("test.source"));////// debug 20190505
 		  try
 		  {
 			  //create new result folder and subfolders if need.
@@ -199,7 +200,8 @@ public class TestManager extends TestBase implements IComFolder {
 	  @BeforeClass(dependsOnMethods="beforeClass")
 	  @Parameters({"indexAppServers", "indexDBServers", "indexToolsetDBServers", "selectLanguage"})
 	  public void beforeClassInTestManager(ITestContext context, @Optional String indexAppServers, @Optional String indexDBServers, @Optional String indexToolsetDBServers,@Optional String dateFormat) throws Exception {
-		  logger.info(getClass().getName()+" beforeClass-setUpTest running!"); 
+		  logger.info(getClass().getName()+" beforeClass-setUpTest running!");
+		  System.out.println(System.getProperty("test.source"));////// debug 20190505
 		  indexAppServer=changeStringToInt(indexAppServers,0);
 		  indexDBServer=changeStringToInt(indexDBServers,0);
 		  indexToolsetDBServer=changeStringToInt(indexToolsetDBServers,0);
